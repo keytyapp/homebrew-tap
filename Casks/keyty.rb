8ce0902d00ca51fc899f3361bbb6d807263bb23b
Serhii Bykov <esphynox@gmail.com>
2026-09-20 12:57:54 +0200

    Remove deprecated URL verification (#15)

--- Casks/keyty.rb
+++ Casks/keyty.rb
@@ -2,8 +2,7 @@ cask "keyty" do
   version "1.7.0"
   sha256 "437acdd76ab6ba8e4ae9d5bfd2de0194342250f2c4446522c36d4c4b0b121b40"
 
-  url "https://github.com/keytyapp/Keyty/releases/download/v#{version}/Keyty.dmg",
-      verified: "github.com/keytyapp/Keyty/"
+  url "https://github.com/keytyapp/Keyty/releases/download/v#{version}/Keyty.dmg"
   name "Keyty"
   desc "Visualize keyboard and mouse input"
   homepage "https://keyty.app/"
